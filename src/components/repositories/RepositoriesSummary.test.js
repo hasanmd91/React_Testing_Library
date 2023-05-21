ddba1359e26ce07e75ea91d0925e2aa6d1f53fd3
hasanmd91 <hasanmd1991@hotmail.com>
2023-05-21 16:34:17 +0300

    build flexiable queries

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -5,11 +5,14 @@ test("displays the primary language of the repositories", () => {
   const repository = {
     stargazers_count: 5,
     open_issues: 1,
-    forks: 5,
+    forks: 6,
     language: "javaScript",
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("javaScript");
-  expect(language).toBeInTheDocument();
+  for (let key in repository) {
+    const value = repository[key];
+    const item = screen.getByText(new RegExp(value));
+    expect(item).toBeInTheDocument();
+  }
 });
